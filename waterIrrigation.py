9971b7cf59bb0f8bcc7d4b79d1e885e68664f4f6
Charlie Hernandez <justdaft13@gmail.com>
2023-01-23 18:25:03 -0800

    Create waterIrrigation.py
    first commit

---
+++ waterIrrigation.py
@@ -0,0 +1,26 @@
+import time
+import keyboard
+
+soil_moisture = 10
+meter = "##########"
+
+print("This is a simple irrigation simulation")
+
+while True:
+    print("Soil Moisture", end=" ")
+    print("\tMeter")
+    print(soil_moisture, end=" ")
+    print("\t\t\t\t" + meter[0:soil_moisture])
+
+    if not keyboard.is_pressed('w'):
+        if soil_moisture != 0:
+            soil_moisture -= 1
+        else:
+            print("WARNING! SOIL MOISTURE LOW")
+
+    if keyboard.is_pressed('w') and soil_moisture != 10:
+        soil_moisture += 1
+
+    time.sleep(1.5)
+
+
