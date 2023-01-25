173cae9c72261ba8fa9a83a0c32d2908ccf30a22
Charlie Hernandez <justdaft13@gmail.com>
2023-01-25 12:46:37 -0800

    added humidity and temp simulation

---
+++ humiditySim.py
@@ -0,0 +1,27 @@
+import random
+import time
+
+# Initial humidity
+humidity = 60
+
+# Humidity range
+min_humidity = 20
+max_humidity = 90
+
+# Humidity change range
+min_change = -3
+max_change = 3
+
+while True:
+    # Generate a random humidity change
+    humidity_change = random.randint(min_change, max_change)
+    humidity += humidity_change
+
+    # Check if humidity is within range
+    if humidity > max_humidity:
+        humidity = max_humidity
+    elif humidity < min_humidity:
+        humidity = min_humidity
+
+    print("Humidity:", humidity)
+    time.sleep(1)
---
+++ temperatureSim.py
@@ -0,0 +1,27 @@
+import random
+import time
+
+# Initial temperature
+temp = 72.0
+
+# Temperature range
+min_temp = 32.0
+max_temp = 100.0
+
+# Temperature change range
+min_change = -4
+max_change = 4
+
+while True:
+    # Generate a random temperature change
+    temp_change = random.randint(min_change, max_change)
+    temp += temp_change
+
+    # Check if temperature is within range
+    if temp > max_temp:
+        temp = max_temp
+    elif temp < min_temp:
+        temp = min_temp
+
+    print("Temperature:", temp)
+    time.sleep(1)
