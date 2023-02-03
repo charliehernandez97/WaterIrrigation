70c250e14dbc93a9eeae7f16c17a74ebaf49fcf8
Charlie Hernandez <justdaft13@gmail.com>
2023-02-03 04:01:59 -0800

    first plc demo completed

--- PLC/build/POUS.c
+++ PLC/build/POUS.c
@@ -201,20 +201,54 @@ __end:
 
 
 void WATERIRRIGATION_init__(WATERIRRIGATION *data__, BOOL retain) {
-  __INIT_VAR(data__->VALVE,__BOOL_LITERAL(FALSE),retain)
-  __INIT_VAR(data__->PUMP,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->OUTGOING_VALVE,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->INCOMING_VALVE,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->OUTGOING_PUMP,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->INCOMING_PUMP,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->TANK_FULL,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->PULSE,__BOOL_LITERAL(FALSE),retain)
+  __INIT_VAR(data__->MAX_TANK_LEVEL,10,retain)
+  __INIT_VAR(data__->TANK_LEVEL_VALUE,10,retain)
+  CTD_init__(&data__->CTD0,retain);
+  CTU_init__(&data__->CTU0,retain);
+  TON_init__(&data__->TON0,retain);
+  TOF_init__(&data__->TOF0,retain);
   TP_init__(&data__->TP0,retain);
-  __INIT_VAR(data__->PULSE,__time_to_timespec(1, 10000, 0, 0, 0, 0),retain)
+  R_TRIG_init__(&data__->R_TRIG1,retain);
+  R_TRIG_init__(&data__->R_TRIG2,retain);
 }
 
 // Code part
 void WATERIRRIGATION_body__(WATERIRRIGATION *data__) {
   // Initialise TEMP variables
 
-  __SET_VAR(data__->TP0.,IN,,__GET_VAR(data__->VALVE,));
-  __SET_VAR(data__->TP0.,PT,,__GET_VAR(data__->PULSE,));
+  __SET_VAR(data__->TP0.,IN,,__GET_VAR(data__->OUTGOING_VALVE,));
+  __SET_VAR(data__->TP0.,PT,,__time_to_timespec(1, 10000, 0, 0, 0, 0));
   TP_body__(&data__->TP0);
-  __SET_VAR(data__->,PUMP,,__GET_VAR(data__->TP0.Q,));
+  __SET_VAR(data__->,OUTGOING_PUMP,,__GET_VAR(data__->TP0.Q,));
+  __SET_VAR(data__->R_TRIG1.,CLK,,(__GET_VAR(data__->PULSE,) && __GET_VAR(data__->OUTGOING_PUMP,)));
+  R_TRIG_body__(&data__->R_TRIG1);
+  __SET_VAR(data__->CTD0.,CD,,__GET_VAR(data__->R_TRIG1.Q,));
+  __SET_VAR(data__->CTD0.,LD,,__GET_VAR(data__->TANK_FULL,));
+  __SET_VAR(data__->CTD0.,PV,,__GET_VAR(data__->MAX_TANK_LEVEL,));
+  CTD_body__(&data__->CTD0);
+  __SET_VAR(data__->,INCOMING_PUMP,,__GET_VAR(data__->CTD0.Q,));
+  __SET_VAR(data__->,TANK_LEVEL_VALUE,,__GET_VAR(data__->CTD0.CV,));
+  __SET_VAR(data__->R_TRIG2.,CLK,,(__GET_VAR(data__->PULSE,) && __GET_VAR(data__->INCOMING_PUMP,)));
+  R_TRIG_body__(&data__->R_TRIG2);
+  __SET_VAR(data__->CTU0.,CU,,__GET_VAR(data__->R_TRIG2.Q,));
+  __SET_VAR(data__->CTU0.,R,,__GET_VAR(data__->OUTGOING_PUMP,));
+  __SET_VAR(data__->CTU0.,PV,,__GET_VAR(data__->MAX_TANK_LEVEL,));
+  CTU_body__(&data__->CTU0);
+  __SET_VAR(data__->,TANK_FULL,,__GET_VAR(data__->CTU0.Q,));
+  __SET_VAR(data__->,TANK_LEVEL_VALUE,,__GET_VAR(data__->CTU0.CV,));
+  __SET_VAR(data__->TON0.,IN,,!(__GET_VAR(data__->PULSE,)));
+  __SET_VAR(data__->TON0.,PT,,__time_to_timespec(1, 500, 0, 0, 0, 0));
+  TON_body__(&data__->TON0);
+  __SET_VAR(data__->TOF0.,IN,,__GET_VAR(data__->TON0.Q,));
+  __SET_VAR(data__->TOF0.,PT,,__time_to_timespec(1, 500, 0, 0, 0, 0));
+  TOF_body__(&data__->TOF0);
+  __SET_VAR(data__->,PULSE,,__GET_VAR(data__->TOF0.Q,));
 
   goto __end;
 
